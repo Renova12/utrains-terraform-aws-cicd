9ee1b699aa4d22ce224eff1d206da7b0877a1d5a
hermann90 <hermannchefouet@gmail.com>
2021-11-13 17:37:43 +0400

    CICD Utrains Pipeline Initial version

---
+++ cicd-pipeline.tf
@@ -0,0 +1,116 @@
+#this is the file that declares all the resources that terraform will build in aws.
+resource "aws_codebuild_project" "tf-plan" {
+  name          = "tf-cicd-plan2"
+  description   = "Plan stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+    #declaration of the version of the terraform atellecharger image in Git Hub. 
+    #we notice that we attribute the accesses and we specify the roles and credentials for this download.
+    #this content will be launched by a yml (plan-buildspec.yml) file
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:0.14.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+    #allows to execute the terraform plan command, once our codes are updated on the master branch in github.
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/plan-buildspec.yml")
+ }
+}
+
+resource "aws_codebuild_project" "tf-apply" {
+  name          = "tf-cicd-apply"
+  description   = "Apply stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:0.14.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+    #file allows to execute the terraform apply --auto-approve command once our codes are updated in the main branch.
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/apply-buildspec.yml")
+ }
+}
+
+
+resource "aws_codepipeline" "cicd_pipeline" {
+
+    name = "tf-cicd"
+    role_arn = aws_iam_role.tf-codepipeline-role.arn
+
+    artifact_store {
+        type="S3"
+        location = aws_s3_bucket.codepipeline_artifacts.id
+    }
+
+    stage {
+        name = "Source"
+        action{
+            name = "Source"
+            category = "Source"
+            owner = "AWS"
+            provider = "CodeStarSourceConnection"
+            version = "1"
+            output_artifacts = ["tf-code"]
+            configuration = {
+                FullRepositoryId = "davoclock/aws-cicd-pipeline"
+                BranchName   = "master"
+                ConnectionArn = var.codestar_connector_credentials
+                OutputArtifactFormat = "CODE_ZIP"
+            }
+        }
+    }
+
+    stage {
+        name ="Plan"
+        action{
+            name = "Build"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-cicd-plan"
+            }
+        }
+    }
+
+    stage {
+        name ="Deploy"
+        action{
+            name = "Deploy"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-cicd-apply"
+            }
+        }
+    }
+
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+dockerhub_credentials = "arn:aws:secretsmanager:us-east-1:076892551558:secret:codebuild/dockerhubcode-iurf6a"
+codestar_connector_credentials = "arn:aws:codestar-connections:us-east-1:076892551558:connection/19624f37-d6e3-4572-97d0-8bff85b75b29"
